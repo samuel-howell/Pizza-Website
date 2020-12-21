2e7b6512e19218ab0ffb4653f71f6532dbba7a2d
Samuel Howell <60439628+SlideHowell@users.noreply.github.com>
2020-12-21 16:36:14 -0500

    adding more queries

--- php/placeOrder_page.php
+++ php/placeOrder_page.php
@@ -7,6 +7,16 @@ $lname = $_POST['lname'];
 $address = $_POST['address'];
 $zip = $_POST['zip'];
 
+if(isset($_POST['orderType']))
+{
+    $orderType = $_POST['orderType'];
+}
+
+if(isset($_POST['toppingType']))
+{
+    $toppingType = $_POST['toppingType'];
+}
+
 
 if (!empty($fname)){
     if(!empty($lname)){
@@ -15,10 +25,20 @@ if (!empty($fname)){
 
                 include "connect.php"; // use the database connection file.
 
-                $query = "INSERT INTO customer (First_Name, Last_Name, Address, ZIP) VALUES ('$fname', '$lname', '$address', '$zip')";    // create a query to insert into "customer" table
+                $query1 = "INSERT INTO customer (First_Name, Last_Name, Address, ZIP) VALUES ('$fname', '$lname', '$address', '$zip')";    // create a query to insert into "customer" table
+                $query2 = "INSERT INTO pizza (topping_type) VALUES ('$toppingType')";
+                $query3 = "INSERT INTO pizzaorder (order_type) VALUES ('$orderType')";  // why is this one line not working??!!!!
 
-                //  if query insertion is successful
-                if(mysqli_query($conn, $query))
+                if(mysqli_query($conn, $query3))
+                {
+                    echo "query 3 success";
+                }
+                else
+                {
+                   echo $orderType;
+                }
+                //  if query insertion is successful (mysqli_query() is an execute command basically)
+                if(mysqli_query($conn, $query1) && mysqli_query($conn, $query2) )
                 {
                     echo "<script type = 'text/javascript'>alert('Success'); </script>"; // display alert message over page
                 }
